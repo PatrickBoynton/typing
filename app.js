04f6e70a52686035bdd78ba9f4696a544bf47ac9
PatrickBoynton <jpboynton3@gmail.com>
2021-05-30 01:22:15 -0400

    Added event listeners for changing key colors.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const p = document.querySelectorAll('p');
 
+document.addEventListener('keydown', (event) => {
+  p.forEach((p) => {
+    if (p.classList.contains(event.key)) p.style.backgroundColor = 'yellow';
+  });
+});
+
+document.addEventListener('keyup', (event) => {
+  p.forEach((p) => {
+    if (p.classList.contains(event.key)) p.style.backgroundColor = '#fff';
+  });
+});
