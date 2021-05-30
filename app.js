abb4aa7533bd62a54c4168382a46e232941571e2
PatrickBoynton <jpboynton3@gmail.com>
2021-05-30 01:35:40 -0400

    Added checks for the code.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const p = document.querySelectorAll('p');
 
 document.addEventListener('keydown', (event) => {
+  console.log(event);
   p.forEach((p) => {
     if (p.classList.contains(event.key)) p.style.backgroundColor = 'yellow';
+    if (p.classList.contains(event.code)) p.style.backgroundColor = 'yellow';
   });
 });
 
 document.addEventListener('keyup', (event) => {
   p.forEach((p) => {
     if (p.classList.contains(event.key)) p.style.backgroundColor = '#fff';
+    if (p.classList.contains(event.code)) p.style.backgroundColor = '#fff';
   });
 });
